e8849a8ae0389baad6de60d79b49a4b50791e293
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-15 13:48:58 -0300

    Trabalhando com os parametros de uma requisição

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Param } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+    @Get()
+    findAll() {
+        return 'Listagem de cursos';
+    }
+
+    @Get(':id/:name')
+    findOne(@Param('id') id: string) {
+        return  `Curso com ID ${id}`;
+    }
+} 
+
