dc7b818cca096a56a66104ba9872ca312d1ab7e1
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-15 15:11:47 -0300

    Trabalhandfo com os services 1

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post, Res } from '@nestjs/common';
 import { response } from 'express';
+import { CoursesService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+    constructor(private readonly courseService: CoursesService) {}
     @Get()
     findAll(@Res() response) {
         return  response.status(200).json({message: 'Listagem de cursos' });
