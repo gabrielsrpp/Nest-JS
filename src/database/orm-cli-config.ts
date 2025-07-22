6708e15dff0f3d53ce386740113e6162edc3fa26
gabrielsrpp <gabrielpires2003@hotmail.com>
2025-07-22 13:46:17 -0300

    feat: criando a tabela tags atraves de migracao
    criando a tabela tags atraves de migracao

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from "typeorm";
 import { dataSourceOptions } from "./database.module";
 import { CreateCoursesTable1753191937106 } from "src/migrations/1753191937106-CreateCoursesTable";
+import { CreateTagsTable1753196144680 } from "src/migrations/1753196144680-CreateTagsTable";
 
 export const dataSource = new DataSource({
     ...dataSourceOptions,
     synchronize: false,
-    migrations: [CreateCoursesTable1753191937106],
+    migrations: [CreateCoursesTable1753191937106, CreateTagsTable1753196144680],
 })
